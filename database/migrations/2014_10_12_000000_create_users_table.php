a22ac51c0fde49b687f3cb258280a5de0398a12c
Alexis Yepez <alexis.yepez01@epn.edu.ec>
2021-12-14 19:30:14 -0500

    Modelos y migraciones de las entidades

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nickname')->default('Admin');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('Administrador');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2021_12_14_184252_create_records_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('app_name');
+            $table->string('window_name');
+            $table->string('event_type');
+            $table->boolean('archived');
+            $table->date('date');
+            $table->time('time');
+            $table->text('content');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+}
